6d4f3f89318d66ea241ddfe7bd66cabec722f2e5
Chezilraj <chezilraj.chandran@borngroup.com>
2021-08-05 21:49:13 +0530

    server.js modification

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express')
+// const express = require('express')
+// const serveStatic = require('serve-static')
+// const path = require('path')
+
+// const app = express()
+
+// app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// const port = process.env.PORT || 8080
+// app.listen(port)
+
+
+const express = require("express");
 const serveStatic = require('serve-static')
 const path = require('path')
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 8080
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8080
-app.listen(port)
+// Add Access Control Allow Origin headers
+app.use((res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://benius.herokuapp.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
